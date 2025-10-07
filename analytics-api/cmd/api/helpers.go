234d1b58ff03bbf1666836dd68114988c22408ac
Taylor <taylor.r.meador@gmail.com>
2025-10-07 15:44:51 -0600

    Return JSON responses from /system endpoints

--- analytics-api/cmd/api/helpers.go
+++ analytics-api/cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -19,4 +20,28 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+// Writes data as JSON for uniform responses
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// Marshal data into JSON
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	// Include headers
+	for key, values := range headers {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
--- analytics-api/cmd/api/system.go
+++ analytics-api/cmd/api/system.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // Writes information about the application status, operating environment and version.
 func (app *application) systemHealthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
 
 // Writes system-wide statistics and metrics.
 func (app *application) systemStatsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "total_readings: TODO")
-	fmt.Fprintln(w, "unique_devices: TODO")
+	data := map[string]string{
+		"total_readings":  "TODO",
+		"unique_devices":  "TODO",
+		"total_anomalies": "TODO",
+		"latest_reading":  "TODO",
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
